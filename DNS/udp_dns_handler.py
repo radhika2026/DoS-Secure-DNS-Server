d9172012ae87ca5af9ef2be955ec2769bc477f4d
Harshil Bhandari <harshilbhandari01@gmail.com>
2024-11-18 16:50:01 -0800

    Describe your changes here

--- DNS/udp_dns_handler.py
+++ DNS/udp_dns_handler.py
@@ -1,6 +1,17 @@
 import socket
-from DNS.dns_handler import MyDNSHandler
 import dns.message
+import dns.resolver
+import logging
+import time
+from DNS.dns_handler import MyDNSHandler
+
+# Configure logging
+logging.basicConfig(
+    filename="/Users/harshil/Development/Dev_tests/folder_1/DoS-Secure-DNS-Server/baseline_experimentation/logs/udp_handler.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
 
 class MyUDPDNSHandler(MyDNSHandler):
     def __init__(self, forwarding_server="1.1.1.1", zone_file_path="./zones/test_primary.zone",
@@ -9,50 +20,74 @@ class MyUDPDNSHandler(MyDNSHandler):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket.bind((listen_address, port))
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 65536)
-        # self.socket.settimeout(5)
 
     def run(self):
         try:
             while True:
                 try:
+                    # Receive data from the client
                     data, addr = self.socket.recvfrom(4096)
+                    client_ip, client_port = addr
                 except ConnectionResetError as e:
-                    print(f"Connection reset by peer: {e}")
+                    logging.error(f"Connection reset by peer: {e}")
                     continue
 
-                message = ""
+                # Try to decode as a zone transfer message
                 try:
                     message = data.decode('utf-8')
-                except Exception:
-                    print("dns query received")
+                    if message.split(' ')[0] == "ZONE_TRANSFER":
+                        zone_name = message.split(' ')[1]
+                        target_host = message.split(' ')[2]
+                        target_port = int(message.split(' ')[3])
+                        try:
+                            self.handle_zone_transfer(zone_name, target_host, target_port)
+                            logging.info(f"Zone Transfer successful: {zone_name} to {target_host}:{target_port}")
+                            self.socket.sendto("SUCCESS".encode(), addr)
+                        except Exception as e:
+                            logging.error(f"Zone Transfer failed: {zone_name} to {target_host}:{target_port} - {str(e)}")
+                            self.socket.sendto("FAILURE".encode(), addr)
+                        finally:
+                            continue
+                except UnicodeDecodeError:
+                    # If not a zone transfer message, treat as a DNS query
+                    logging.info(f"DNS query received from {client_ip}:{client_port}")
 
-                if message.split(' ')[0] == "ZONE_TRANSFER":
-                    try:
-                        self.handle_zone_transfer(message.split(' ')[1], message.split(' ')[2],
-                                                  int(message.split(' ')[3]))
-                        print("Zone Transfer successful")
-                        self.socket.sendto("SUCCESS".encode(), addr)
-                    except Exception:
-                        print("Zone Transfer failed")
-                        self.socket.sendto("FAILURE".encode(), addr)
-                    finally:
-                        continue
-
-                # print(request)
+                # Process the DNS query
                 try:
                     request = dns.message.from_wire(data)
+                    query_name = str(request.question[0].name)
+                    query_type = dns.rdatatype.to_text(request.question[0].rdtype)
+
+                    logging.info(f"Received query: {query_name} ({query_type}) from {client_ip}:{client_port}")
+                    start_time = time.time()
+
+                    # Resolve the query
                     reply = self.resolve(request)
+
+                    end_time = time.time()
+                    response_time = end_time - start_time
+
+                    logging.info(
+                        f"Query resolved: {query_name} ({query_type}) for {client_ip}:{client_port} in {response_time:.3f} seconds"
+                    )
                 except dns.resolver.NXDOMAIN:
-                    print("NOT FOUND")
+                    logging.warning(f"Query not found: {query_name} from {client_ip}:{client_port}")
+                    continue
+                except Exception as e:
+                    logging.error(f"Failed to process query from {client_ip}:{client_port} - {str(e)}")
                     continue
 
-                if (dns.rdatatype.to_text(request.question[0].rdtype) == "AXFR" or
-                        dns.rdatatype.to_text(request.question[0].rdtype) == "IXFR"):
-                    self.socket.sendto(reply.to_wire(), addr)
+                # Handle AXFR/IXFR responses
+                if query_type in ["AXFR", "IXFR"]:
+                    try:
+                        self.socket.sendto(reply.to_wire(), addr)
+                        logging.info(f"Zone transfer response sent for {query_name} to {client_ip}:{client_port}")
+                    except Exception as e:
+                        logging.error(f"Failed to send zone transfer response: {str(e)}")
                     continue
 
+                # Build and send the DNS response
                 response = dns.message.make_response(request)
-
                 if reply is not None:
                     if hasattr(reply, 'rrset'):
                         response.answer.append(reply.rrset)
@@ -61,9 +96,87 @@ class MyUDPDNSHandler(MyDNSHandler):
                 else:
                     response.set_rcode(dns.rcode.NXRRSET)
 
-                self.socket.sendto(response.to_wire(), addr)
+                try:
+                    self.socket.sendto(response.to_wire(), addr)
+                    logging.info(f"Response sent for {query_name} to {client_ip}:{client_port}")
+                except Exception as e:
+                    logging.error(f"Failed to send response to {client_ip}:{client_port} - {str(e)}")
 
         except KeyboardInterrupt:
-            pass
+            logging.info("UDP DNS handler shutting down")
         finally:
             self.socket.close()
+
+
+
+
+# # OG code
+# import socket
+# from DNS.dns_handler import MyDNSHandler
+# import dns.message
+
+# class MyUDPDNSHandler(MyDNSHandler):
+#     def __init__(self, forwarding_server="1.1.1.1", zone_file_path="./zones/test_primary.zone",
+#                  private_key_path="./keys/primary.pem", listen_address="", port=31111):
+#         super().__init__(forwarding_server, zone_file_path, private_key_path)
+#         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#         self.socket.bind((listen_address, port))
+#         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 65536)
+#         # self.socket.settimeout(5)
+
+#     def run(self):
+#         try:
+#             while True:
+#                 try:
+#                     data, addr = self.socket.recvfrom(4096)
+#                 except ConnectionResetError as e:
+#                     print(f"Connection reset by peer: {e}")
+#                     continue
+
+#                 message = ""
+#                 try:
+#                     message = data.decode('utf-8')
+#                 except Exception:
+#                     print("dns query received")
+
+#                 if message.split(' ')[0] == "ZONE_TRANSFER":
+#                     try:
+#                         self.handle_zone_transfer(message.split(' ')[1], message.split(' ')[2],
+#                                                   int(message.split(' ')[3]))
+#                         print("Zone Transfer successful")
+#                         self.socket.sendto("SUCCESS".encode(), addr)
+#                     except Exception:
+#                         print("Zone Transfer failed")
+#                         self.socket.sendto("FAILURE".encode(), addr)
+#                     finally:
+#                         continue
+
+#                 # print(request)
+#                 try:
+#                     request = dns.message.from_wire(data)
+#                     reply = self.resolve(request)
+#                 except dns.resolver.NXDOMAIN:
+#                     print("NOT FOUND")
+#                     continue
+
+#                 if (dns.rdatatype.to_text(request.question[0].rdtype) == "AXFR" or
+#                         dns.rdatatype.to_text(request.question[0].rdtype) == "IXFR"):
+#                     self.socket.sendto(reply.to_wire(), addr)
+#                     continue
+
+#                 response = dns.message.make_response(request)
+
+#                 if reply is not None:
+#                     if hasattr(reply, 'rrset'):
+#                         response.answer.append(reply.rrset)
+#                     else:
+#                         response.answer.append(reply)
+#                 else:
+#                     response.set_rcode(dns.rcode.NXRRSET)
+
+#                 self.socket.sendto(response.to_wire(), addr)
+
+#         except KeyboardInterrupt:
+#             pass
+#         finally:
+#             self.socket.close()
--- dns_gatekeeper.py
+++ dns_gatekeeper.py
@@ -5,6 +5,14 @@ import time
 from concurrent.futures import ThreadPoolExecutor
 import uuid
 import argparse
+import logging
+
+# Configure logging
+logging.basicConfig(
+    filename=r"/Users/harshil/Development/Dev_tests/folder_1/DoS-Secure-DNS-Server/baseline_experimentation/logs/dns_gatekeeper.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 
 class MyDNSGatekeeper:
@@ -22,21 +30,29 @@ class MyDNSGatekeeper:
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket.bind((listen_address, port))
 
-    def resolve(self, request):
+    def resolve(self, request, client_ip):
         query_name = str(request.question[0].name)
         query_type = dns.rdatatype.to_text(request.question[0].rdtype)
+        start_time = time.time()
 
+        # Determine server based on random key
         key = uuid.uuid4().int
-
-        if hasattr(request, 'update') and len(request.update):
-            return self.add_record(request)
-
         if key % 2 == 0:
-            print("Forwarding request to primary server")
-            return self.forward_query(query_name, query_type, self.primary_ns_host, self.primary_ns_port)
+            target_host, target_port = self.primary_ns_host, self.primary_ns_port
+            logging.info(f"Routing query to primary server: {query_name} ({query_type}) from {client_ip}")
         else:
-            print("Forwarding request to secondary server")
-            return self.forward_query(query_name, query_type, self.secondary_ns_host, self.secondary_ns_port)
+            target_host, target_port = self.secondary_ns_host, self.secondary_ns_port
+            logging.info(f"Routing query to secondary server: {query_name} ({query_type}) from {client_ip}")
+
+        try:
+            response = self.forward_query(query_name, query_type, target_host, target_port)
+            end_time = time.time()
+            latency = end_time - start_time
+            logging.info(f"Query resolved: {query_name} ({query_type}) in {latency:.3f} seconds")
+            return response
+        except Exception as e:
+            logging.error(f"Failed to resolve query: {query_name} ({query_type}) - {str(e)}")
+            raise
 
     def forward_query(self, query_name, query_type, host, port):
         resolver = dns.resolver.Resolver()
@@ -44,41 +60,11 @@ class MyDNSGatekeeper:
         resolver.nameservers = [host]
         return resolver.resolve(query_name, dns.rdatatype.from_text(query_type))
 
-    def zone_transfer(self, host, port, target_host, target_port):
-        # Create a UDP socket
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-        # Send a message to the server
-        msg = "ZONE_TRANSFER example.com " + target_host + " " + target_port
-        client_socket.sendto(msg.encode('utf-8'), (host, port))
-
-        # Receive the response from the server
-        response, server_address = client_socket.recvfrom(1024)
-
-        # Decode and print the response
-        decoded_response = response.decode('utf-8')
-        print(f"Response from server {server_address}: {decoded_response}")
-
-        # Close the socket
-        client_socket.close()
-
-    def add_record(self, request):
-        update = dns.update.Update(request.zone[0].name)
-
-        address = [rd for rd in request.update[0].items][0].address
-
-        update.add(request.update[0].name, 300, request.update[0].rdtype, address)
-
-        dns.query.udp(update, self.primary_ns_host, port=self.primary_ns_port)
-
-        print("Added record to Primary nameserver")
-
-        return request.update[0]
-
     def validate(self, sender_ip):
         if sender_ip in self.history:
             self.history[sender_ip] += 1
             if self.history[sender_ip] > 100:
+                logging.warning(f"IP {sender_ip} exceeded rate limit")
                 return False
             return True
         else:
@@ -90,7 +76,7 @@ class MyDNSGatekeeper:
             while True:
                 time.sleep(5)
                 self.history = {}
-                print("Reset history done")
+                logging.info("Rate limiting history reset")
         except KeyboardInterrupt:
             pass
 
@@ -98,18 +84,17 @@ class MyDNSGatekeeper:
         try:
             while True:
                 data, addr = self.socket.recvfrom(4096)
+                client_ip = addr[0]
 
-                if not self.validate(addr[0]):
-                    print("IP banned: ", addr[0])
+                if not self.validate(client_ip):
+                    logging.warning(f"Blocked IP: {client_ip}")
                     self.socket.sendto("IP banned".encode(), addr)
                     continue
 
                 request = dns.message.from_wire(data)
-                # print(request)
                 try:
-                    reply = self.resolve(request)
+                    reply = self.resolve(request, client_ip)
                 except Exception:
-                    print("DNS ERROR")
                     continue
 
                 response = dns.message.make_response(request)
@@ -120,7 +105,6 @@ class MyDNSGatekeeper:
                     response.answer.append(reply)
 
                 self.socket.sendto(response.to_wire(), addr)
-
         except KeyboardInterrupt:
             pass
         finally:
@@ -130,27 +114,200 @@ class MyDNSGatekeeper:
         try:
             while True:
                 time.sleep(100)
-                print("Performing a zone tranfer")
+                logging.info("Initiating zone transfer")
                 self.zone_transfer(self.secondary_ns_host, self.secondary_ns_port,
                                    self.primary_ns_host, self.primary_ns_port)
         except KeyboardInterrupt:
             pass
 
+    def zone_transfer(self, host, port, target_host, target_port):
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        msg = f"ZONE_TRANSFER example.com {target_host} {target_port}"
+        client_socket.sendto(msg.encode('utf-8'), (host, port))
+
+        try:
+            response, server_address = client_socket.recvfrom(1024)
+            decoded_response = response.decode('utf-8')
+            logging.info(f"Zone transfer response from {server_address}: {decoded_response}")
+        except socket.timeout:
+            logging.error("Zone transfer timed out")
+        finally:
+            client_socket.close()
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--port", type=int, default=31110, help="specify dns gatekeeper port")
-    parser.add_argument("--primary_ns_host", default="127.0.0.1", help="specify primary dns host")
-    parser.add_argument("--primary_ns_port", type=int, default=31111, help="specify primary dns port")
-    parser.add_argument("--secondary_ns_host", default="127.0.0.1", help="specify secondary dns host")
-    parser.add_argument("--secondary_ns_port", type=int, default=31112, help="specify secondary dns port")
+    parser.add_argument("--port", type=int, default=31110, help="Specify DNS gatekeeper port")
+    parser.add_argument("--primary_ns_host", default="127.0.0.1", help="Specify primary DNS host")
+    parser.add_argument("--primary_ns_port", type=int, default=31111, help="Specify primary DNS port")
+    parser.add_argument("--secondary_ns_host", default="127.0.0.1", help="Specify secondary DNS host")
+    parser.add_argument("--secondary_ns_port", type=int, default=31112, help="Specify secondary DNS port")
     args = parser.parse_args()
 
     resolver = MyDNSGatekeeper(args.primary_ns_host, args.primary_ns_port, args.secondary_ns_host,
                                args.secondary_ns_port, port=args.port)
 
     executor = ThreadPoolExecutor(3)
-
     executor.submit(resolver.run)
     executor.submit(resolver.perform_zone_transfers)
     executor.submit(resolver.reset_history)
+
+
+# # OG Code
+
+# import dns.message
+# import dns.resolver
+# import socket
+# import time
+# from concurrent.futures import ThreadPoolExecutor
+# import uuid
+# import argparse
+
+
+# class MyDNSGatekeeper:
+#     def __init__(self, primary_ns_host="127.0.0.1", primary_ns_port=31111,
+#                  secondary_ns_host="127.0.0.1", secondary_ns_port=31112, listen_address="", port=31110):
+#         super().__init__()
+#         self.primary_ns_host = primary_ns_host
+#         self.secondary_ns_host = secondary_ns_host
+
+#         self.primary_ns_port = primary_ns_port
+#         self.secondary_ns_port = secondary_ns_port
+
+#         self.history = {}
+
+#         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#         self.socket.bind((listen_address, port))
+
+#     def resolve(self, request):
+#         query_name = str(request.question[0].name)
+#         query_type = dns.rdatatype.to_text(request.question[0].rdtype)
+
+#         key = uuid.uuid4().int
+
+#         if hasattr(request, 'update') and len(request.update):
+#             return self.add_record(request)
+
+#         if key % 2 == 0:
+#             print("Forwarding request to primary server")
+#             return self.forward_query(query_name, query_type, self.primary_ns_host, self.primary_ns_port)
+#         else:
+#             print("Forwarding request to secondary server")
+#             return self.forward_query(query_name, query_type, self.secondary_ns_host, self.secondary_ns_port)
+
+#     def forward_query(self, query_name, query_type, host, port):
+#         resolver = dns.resolver.Resolver()
+#         resolver.port = port
+#         resolver.nameservers = [host]
+#         return resolver.resolve(query_name, dns.rdatatype.from_text(query_type))
+
+#     def zone_transfer(self, host, port, target_host, target_port):
+#         # Create a UDP socket
+#         client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+#         # Send a message to the server
+#         msg = "ZONE_TRANSFER example.com " + target_host + " " + target_port
+#         client_socket.sendto(msg.encode('utf-8'), (host, port))
+
+#         # Receive the response from the server
+#         response, server_address = client_socket.recvfrom(1024)
+
+#         # Decode and print the response
+#         decoded_response = response.decode('utf-8')
+#         print(f"Response from server {server_address}: {decoded_response}")
+
+#         # Close the socket
+#         client_socket.close()
+
+#     def add_record(self, request):
+#         update = dns.update.Update(request.zone[0].name)
+
+#         address = [rd for rd in request.update[0].items][0].address
+
+#         update.add(request.update[0].name, 300, request.update[0].rdtype, address)
+
+#         dns.query.udp(update, self.primary_ns_host, port=self.primary_ns_port)
+
+#         print("Added record to Primary nameserver")
+
+#         return request.update[0]
+
+#     def validate(self, sender_ip):
+#         if sender_ip in self.history:
+#             self.history[sender_ip] += 1
+#             if self.history[sender_ip] > 100:
+#                 return False
+#             return True
+#         else:
+#             self.history[sender_ip] = 1
+#             return True
+
+#     def reset_history(self):
+#         try:
+#             while True:
+#                 time.sleep(5)
+#                 self.history = {}
+#                 print("Reset history done")
+#         except KeyboardInterrupt:
+#             pass
+
+#     def run(self):
+#         try:
+#             while True:
+#                 data, addr = self.socket.recvfrom(4096)
+
+#                 if not self.validate(addr[0]):
+#                     print("IP banned: ", addr[0])
+#                     self.socket.sendto("IP banned".encode(), addr)
+#                     continue
+
+#                 request = dns.message.from_wire(data)
+#                 # print(request)
+#                 try:
+#                     reply = self.resolve(request)
+#                 except Exception:
+#                     print("DNS ERROR")
+#                     continue
+
+#                 response = dns.message.make_response(request)
+
+#                 if hasattr(reply, 'rrset'):
+#                     response.answer.append(reply.rrset)
+#                 else:
+#                     response.answer.append(reply)
+
+#                 self.socket.sendto(response.to_wire(), addr)
+
+#         except KeyboardInterrupt:
+#             pass
+#         finally:
+#             self.socket.close()
+
+#     def perform_zone_transfers(self):
+#         try:
+#             while True:
+#                 time.sleep(100)
+#                 print("Performing a zone tranfer")
+#                 self.zone_transfer(self.secondary_ns_host, self.secondary_ns_port,
+#                                    self.primary_ns_host, self.primary_ns_port)
+#         except KeyboardInterrupt:
+#             pass
+
+
+# if __name__ == "__main__":
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("--port", type=int, default=31110, help="specify dns gatekeeper port")
+#     parser.add_argument("--primary_ns_host", default="127.0.0.1", help="specify primary dns host")
+#     parser.add_argument("--primary_ns_port", type=int, default=31111, help="specify primary dns port")
+#     parser.add_argument("--secondary_ns_host", default="127.0.0.1", help="specify secondary dns host")
+#     parser.add_argument("--secondary_ns_port", type=int, default=31112, help="specify secondary dns port")
+#     args = parser.parse_args()
+
+#     resolver = MyDNSGatekeeper(args.primary_ns_host, args.primary_ns_port, args.secondary_ns_host,
+#                                args.secondary_ns_port, port=args.port)
+
+#     executor = ThreadPoolExecutor(3)
+
+#     executor.submit(resolver.run)
+#     executor.submit(resolver.perform_zone_transfers)
+#     executor.submit(resolver.reset_history)
